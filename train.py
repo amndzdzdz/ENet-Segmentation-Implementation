2c074f2f7bcec5e6f251f4863c6f8c070d5f52dc
amndzdzdz <amd9105@thi.de>
2024-09-02 20:34:40 +0200

    added complete train script

--- train.py
+++ train.py
@@ -1,6 +1,12 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
 writer = SummaryWriter()
+from dataset import GTAData
+from model import ENet
+import argparse
+
 """
 This script contains the train loop for training the ENet
 """
@@ -49,15 +55,39 @@ def eval_one_epoch(epoch, dataloader: DataLoader, device, model, criterion):
     return avg_loss
 
 
-def run(num_epochs, dataloader, device, model, criterion, optimizer):
+def train(num_epochs, train_loader, val_loader, device, model, criterion, optimizer):
     """
     Training script
     """
+    for epoch in range(num_epochs):
+        train_loss = train_one_epoch(epoch, train_loader, device, model, criterion, optimizer)
+        val_loss = eval_one_epoch(epoch, val_loader, device, model, criterion, optimizer)
+        writer.add_scalar("Loss/train", train_loss, epoch)
+        writer.add_scalar("Loss/val", val_loss, epoch)
 
-    return None
+def main(args):
+    num_epochs, lr, batch_size, weight_decay = args.num_epochs, args.lr, args.batch_size, args.weight_decay
 
-def main():
-    return None
+    transforms = None
+    categories = None
+    dataset = GTAData("data", transforms, categories)
+    train_loader, val_loader = create_dataloaders(GTADataset, batch_size=batch_size, test_size=0.2)
+    
+    num_epochs = 50
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    model = ENet(3, len(categories))
+    optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+    criterion = nn.CrossEntropyLoss()
+
+    train(num_epochs, train_loader, val_loader, device, model, criterion, optimizer)
+    
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_epochs", default=50, help='specify how long the model will be trained')
+    parser.add_argument("--lr", default=0.0005, help='specify how long the model will be trained')
+    parser.add_argument("--batch_size", default=10, help='specify how long the model will be trained')
+    args = parser.parse_args()
+    
+    main(args)
