5e4c7ada1503471ebfe828bd557a8f9ae0f1ad0e
amndzdzdz <amd9105@thi.de>
2024-09-04 21:13:06 +0200

    added function that converts the channel dimension into the class

--- utils.py
+++ utils.py
@@ -1,12 +1,18 @@
 import os
+import torch
+import torch.nn.functional as F
+import numpy as np
 from torch.utils.data import random_split, DataLoader
 
 def get_image_names(path):
     image_names = []
     image_dir = os.path.join(path, "images")
+    i = 0
     for imagename in os.listdir(image_dir):
+        i += 1 
         image_names.append(imagename)
-
+        if i > 499:
+            break
     return image_names
 
 def create_dataloaders(dataset, batch_size=16, test_size=0.2):
@@ -23,4 +29,16 @@ def create_dataloaders(dataset, batch_size=16, test_size=0.2):
 
     return train_loader, val_loader
 
+def probability_to_class(tensor, categories):
+    softmaxed_tensor = F.softmax(tensor, dim=1)
+    argmaxed_tensor = softmaxed_tensor.argmax(dim=1)
+    mask = np.array(categories)[argmaxed_tensor]
+
+    return mask
+
+
+if __name__ == "__main__":
+    categories = [0, 7, 8, 11, 12, 13, 17, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33]
+    tensor = torch.rand((1, 19, 512, 512))
+    probability_to_class(tensor, categories)
 
